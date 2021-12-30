4f71417122c23bc062f12f5d212103ec1d76f0a0
Gabriel Donato <gabrielcdonato@gmail.com>
2021-12-29 23:29:53 -0300

    fix: typescript dependecies

--- App.tsx
+++ App.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Welcome } from './src/components/Welcome';
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Welcome title='React Native Bare Workflow com Typescript'/>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  return <Dashboard/>
+  
+}
